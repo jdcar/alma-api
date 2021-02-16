3c6f11eb52c98a470483adb9fc7925a28bf16172
jdcar <jdcarlstone@gmail.com>
2021-02-16 13:15:40 -0600

    Call bibs API to get xml of bib record

--- index.py
+++ index.py
@@ -1,3 +1,25 @@
 import config
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
 
-print(config.api_key)
+# Filepath of mmsids
+with open(r"test-10.csv", newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ')
+
+    for mmsid in reader:
+
+        # mmsid = "9980376370502441"
+
+        mmsid = re.sub(r"(\[')(\d+)('\])", r"\2", str(mmsid))
+
+        queryURL = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/"
+
+        apiKey = config.api_key
+
+        response = requests.get(queryURL + mmsid + "?apikey=" + apiKey )
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        print(soup.record)
